85cb553d6ee2b8637c94b7a431af220b9b79def1
Daniel Rudnitski <daniel.rudnitski@ibm.com>
2019-03-18 01:15:22 -0400

    Setup now uses mv with sudo

--- ci/setup.sh
+++ ci/setup.sh
@@ -3,13 +3,13 @@
 # install sops
 curl -L -s https://github.com/mozilla/sops/releases/download/3.2.0/sops-3.2.0.linux --output sops && \
   chmod +x sops && \
-  mv sops /usr/local/bin/sops
+  sudo mv sops /usr/local/bin/sops
 
 
 # install kubectl
 curl -L -s https://storage.googleapis.com/kubernetes-release/release/v1.13.0/bin/linux/amd64/kubectl --output kubectl && \
   chmod +x kubectl && \
-  mv kubectl /usr/local/bin/kubectl
+  sudo mv kubectl /usr/local/bin/kubectl
 
 # get kubeconfig from digitalocean api
 mkdir "${HOME}/.kube" && \
@@ -21,7 +21,7 @@ mkdir "${HOME}/.kube" && \
 # install helm, helm-diff and helm-secrets
 curl -L -s https://storage.googleapis.com/kubernetes-helm/helm-v2.13.0-linux-amd64.tar.gz --output helm.tar.gz && \
   tar -zxvf helm.tar.gz && \
-  mv linux-amd64/helm /usr/local/bin/helm && \
+  sudo mv linux-amd64/helm /usr/local/bin/helm && \
   helm init --client-only && \
   helm plugin install https://github.com/databus23/helm-diff && \
   helm plugin install https://github.com/futuresimple/helm-secrets
@@ -29,7 +29,7 @@ curl -L -s https://storage.googleapis.com/kubernetes-helm/helm-v2.13.0-linux-amd
 # install helmfile
 curl -L -s https://github.com/roboll/helmfile/releases/download/v0.45.3/helmfile_linux_amd64 --output helmfile && \
   chmod +x helmfile && \
-  mv helmfile /usr/local/bin/helmfile
+  sudo mv helmfile /usr/local/bin/helmfile
 
 # import pgp key
 gpg --import ./ci/pgp/key.asc
